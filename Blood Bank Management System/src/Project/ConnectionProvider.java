1f13a160ce09ea08928bfc58ad30af4bfdd810e1
Darshit <ddsmehta2943@gmail.com>
2025-01-24 14:52:12 +0530

    Initial Commit

---
+++ Blood Bank Management System/src/Project/ConnectionProvider.java
@@ -0,0 +1,24 @@
+ /*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Project;
+import java.sql.*;
+/**
+ *
+ * @author Atharv Talathi
+ */
+public class ConnectionProvider {
+    public static Connection getCon()
+    {
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/bbms","root","darshit@2904");
+            return con;
+        }
+        catch(ClassNotFoundException | SQLException e)
+        {
+            return null;
+        }
+    }
+}
